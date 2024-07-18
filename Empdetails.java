5c84986c5b6692c6cc8ef36db2fe543efb701e1d
Kumar Saurabh <165996271+ksaurabh007@users.noreply.github.com>
2024-07-18 11:53:19 +0530

    Add files via upload

---
+++ Empdetails.java
@@ -0,0 +1,18 @@
+  class Empdetails
+ {
+ 
+public statis voaid main (String[]args);
+{
+String Name="Sumukh";
+String Company Name="Google";
+char Grade='A'
+int LPA=1200000;
+double Salary= 97856.34;
+System.out.println("Details are:"+"\n");
+System.out.println("Namer: " +Name);
+System.out.println("Company: "+Company Name);
+System.out.println("Grade: "+Grade);
+System.out.println("Package: "+Package);
+System.out.println("Salary: "+Salary);
+}
+}
